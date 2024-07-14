acb2ed511c116ba8dbc53d9561a0a3e031f1b7c8
melo254 <owuormalvine75@gmail.com>
2024-07-14 11:11:14 +0300

    back

--- admin/php/test.php
+++
@@ -1,7 +0,0 @@
-<?php 
-$date = date("F jS"); // Get the month and day in "Month DayOrdinal" format
-$date = date("F jS", strtotime($date)); // Ensure proper date parsing
-echo $date; // Output: April 30th
-
-
- ?>
